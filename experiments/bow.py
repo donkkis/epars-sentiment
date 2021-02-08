3daf3e5e24d957aae4a38a3cf9485d7288408468
Panu Aho <panu.aho@wapice.com>
2021-02-08 08:51:07 +0200

    improve logging

--- experiments/bow.py
+++ experiments/bow.py
@@ -1,11 +1,15 @@
 import os
 import mlflow
+import logging
 import preprocess as pr
+import numpy as np
 
 from sklearn.model_selection import KFold
 from sklearn.svm import LinearSVC
 from dotenv import load_dotenv
+
 load_dotenv()
+logging.getLogger("mlflow").setLevel(logging.ERROR)
 
 def _preprocess():
     data = pr.read_data()
@@ -37,8 +41,10 @@ def run():
         acc_train.append(model.score(X_train, y_train))
         acc_test.append(model.score(X_test, y_test))
 
-    print([f'{a:.2f}' for a in acc_train])
-    print([f'{a:.2f}' for a in acc_test])
+    print('Mean train accuracy:', np.mean(acc_train))
+    print('Mean test accuracy:', np.mean(acc_test))
+    mlflow.log_param('folds_train_acc', [f'{a:.3f}' for a in acc_train])
+    mlflow.log_param('folds_test_acc', [f'{a:.3f}' for a in acc_test])
 
 if __name__ == '__main__':
     mlflow.set_tracking_uri(os.getenv('MLFLOW_TRACKING_URI'))
