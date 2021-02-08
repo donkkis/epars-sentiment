1db3a6482c73af2c2b03b7dbfa74c43d4ab6e543
Panu Aho <panu.aho@wapice.com>
2021-02-08 09:37:25 +0200

    make classifier parametrizable

--- experiments/bow.py
+++ experiments/bow.py
@@ -6,10 +6,13 @@ import numpy as np
 
 from sklearn.model_selection import KFold
 from sklearn.svm import LinearSVC
+from sklearn.metrics import balanced_accuracy_score
 from dotenv import load_dotenv
 
 load_dotenv()
 logging.getLogger("mlflow").setLevel(logging.ERROR)
+
+CLASSIFIER = LinearSVC
 
 def _preprocess():
     data = pr.read_data()
@@ -22,10 +25,12 @@ def _preprocess():
     data = pr.to_categorical(data)
     return data
 
-def run():
+def run(classifier=None):
+    global CLASSIFIER
+    if not classifier:
+        classifier = CLASSIFIER
 
     kf = KFold(n_splits=5, shuffle=True, random_state=42)
-    models = []
     acc_train = []
     acc_test = []
 
@@ -34,17 +39,18 @@ def run():
     y = data[['Sentiment']]
 
     for i, (train_idx, test_idx) in enumerate(kf.split(data)):
-        print('Fold', i)
+        print('Fold', i+1)
         X_train, X_test = X.loc[train_idx, :], X.loc[test_idx, :]
         y_train, y_test = y.loc[train_idx, :], y.loc[test_idx, :]
-        model = LinearSVC().fit(X_train, y_train)
-        acc_train.append(model.score(X_train, y_train))
-        acc_test.append(model.score(X_test, y_test))
+        model = classifier().fit(X_train, y_train)
+        acc_train.append(balanced_accuracy_score(model.predict(X_train), y_train))
+        acc_test.append(balanced_accuracy_score(model.predict(X_test), y_test))
 
     print('Mean train accuracy:', np.mean(acc_train))
     print('Mean test accuracy:', np.mean(acc_test))
     mlflow.log_param('folds_train_acc', [f'{a:.3f}' for a in acc_train])
     mlflow.log_param('folds_test_acc', [f'{a:.3f}' for a in acc_test])
+    mlflow.log_artifact(__file__)
 
 if __name__ == '__main__':
     mlflow.set_tracking_uri(os.getenv('MLFLOW_TRACKING_URI'))
